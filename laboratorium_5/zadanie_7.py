03c80e8fba062ca3c1ca9118aa9b9f95e85e5164
MiZych <mzych2001@gmail.com>
2021-11-08 21:54:07 +0100

    more effective

--- laboratorium_5/zadanie_7.py
+++ laboratorium_5/zadanie_7.py
@@ -1,11 +1,11 @@
 number = int(input("Podaj cyfrę: "))
 divisors_list = []
-for i in range(number + 1):
+for i in range((int(number/2)) + 1):
     if i == 0:
         divisors_list.append(i)
     elif number % i == 0:
         divisors_list.append(i)
-
+divisors_list.append(number)
 print("Podzielniki liczby", number, "to:", divisors_list)
 
 
