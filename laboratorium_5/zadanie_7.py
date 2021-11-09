40296dc6477e688b28f983a5cc59a89a49bfd65f
MiZych <mzych2001@gmail.com>
2021-11-09 14:42:20 +0100

    more effective and fixed

--- laboratorium_5/zadanie_7.py
+++ laboratorium_5/zadanie_7.py
@@ -1,13 +1,12 @@
 number = int(input("Podaj cyfrę: "))
 divisors_list = []
-for i in range((int(number/2)) + 1):
-    if i == 0:
-        divisors_list.append(i)
-    elif number % i == 0:
-        divisors_list.append(i)
-divisors_list.append(number)
-print("Podzielniki liczby", number, "to:", divisors_list)
-
-
-
-
+if number == 0:
+    print("Podzielnikami liczby 0 są wszystkie liczby rzeczywiste z wyłączeniem 0")
+else:
+    for i in range((int(number/2)) + 1):
+        if i == 0:
+            pass
+        elif number % i == 0:
+            divisors_list.append(i)
+    divisors_list.append(number)
+    print("Podzielniki liczby", number, "to:", divisors_list)
